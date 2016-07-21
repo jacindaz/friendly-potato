801d176b1c4b5bb2f451fb415588e23e9b3bf70b
Jacinda <jacindaz@gmail.com>
2016-07-20 21:59:48 -0400

    Starter template for product of other numbers

---
+++ product_of_other_numbers.rb
@@ -0,0 +1,8 @@
+def my_function(arg)
+    # write the body of your function here
+    return "running with #{arg}"
+end
+
+# run your function through some test cases here
+# remember: debugging is half the battle!
+puts my_function('test input')
