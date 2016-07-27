4864b405a7342102c2827a59ce1609430a0a613f
Jacinda <jacindaz@gmail.com>
2016-07-26 21:13:33 -0400

    Use greedy approach to save the result of multiply all numbers after a certain index

--- product_of_other_numbers.rb
+++ product_of_other_numbers.rb
@@ -21,7 +21,53 @@ def product_of_other_numbers(numbers)
   # 7 (index 1): 1*3*4 (index 0 * index 2 * index 3)
   # 3 (index 2): 1*7*4 (index 0 * index 1 * index 3)
   # 4 (index 3): 1*7*3 (index 0 * index 1 * index 2)
+
   result
+end
+
+# first pass
+# 1: 7*3*4
+# 7: 3*4
+# 3: 4
+# 4: 1
+#
+# second pass
+# 1: same
+# 7: 1*     (result of first pass)
+# 3: 1*7*   (result of first pass)
+# 4: 1*7*3* (result of first pass)
+
+def better_product_of_other_numbers(numbers_array)
+  array_length = numbers_array.length
+
+  numbers_array.each_with_index do |number, index|
+    numbers_array[index] = multiply_factorial(array_length, numbers_array, index)
+  end
+
+  numbers_array
+end
+
+def multiply_factorial(array_length, numbers_array, index)
+  incrementer = index + 1
+  new_number = 1
+
+  while incrementer < array_length do
+    new_number *= numbers_array[incrementer]
+    incrementer += 1
+  end
+
+  new_number
+end
+
+def print_debugging_message
+  puts "\n==========="
+  puts "index: #{index}" 
+  puts "array_length: #{array_length}"
+  puts "number: #{number}"
+  puts "numbers_array: #{numbers_array}\n"
+
+  puts "resulting array: #{result}"
+  puts "===========\n"
 end
 
 # ASSETS
@@ -33,8 +79,8 @@ end
 
 # run your function through some test cases here
 # remember: debugging is half the battle!
-test_array = [1, 7, 3, 4, 0]
-puts "#{product_of_other_numbers(test_array)}"
+test_array = [1, 7, 3, 4]
+# puts "#{better_product_of_other_numbers(test_array)}"
 
 # your function would return:
 # [84, 12, 28, 21]
