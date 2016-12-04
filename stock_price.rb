9b9fb7a2175f549e1cdabfca12e6c8282f8344c0
Jacinda Zhong <jacindaz@gmail.com>
2016-12-03 18:56:59 -0800

    Better variable names, make method work with negative profits

--- stock_price.rb
+++ stock_price.rb
@@ -1,27 +1,28 @@
-p = [10, 7, 5, 8, 11, 9]
-poop = [100, 50, 0, -50]
-blah = [99, 119, 50, 120, 20]
+profit_six = [10, 7, 5, 8, 11, 9]
+negative_profit = [100, 50, 0, -50]
+profit_seventy = [99, 119, 50, 120, 20]
 
-def max_profit(yesterday_stock_prices, max_profit = 0)
+
+def max_profit(yesterday_stock_prices, max_profit)
   return max_profit if yesterday_stock_prices.length == 1
 
   buy_price = yesterday_stock_prices[0]
   unless buy_price == yesterday_stock_prices[-1]
     yesterday_stock_prices[1..-1].each do |sell_price|
       profit = sell_price - buy_price
-      max_profit = profit if profit > max_profit
 
       # puts "\n=============="
       # puts "buy_price: #{buy_price}, sell_price: #{sell_price}"
       # puts "yesterday_stock_prices: #{yesterday_stock_prices}"
-      # puts "profit: #{profit}, max_profit: #{max_profit}"
+      # puts "profit: #{profit}, max_profit: #{max_profit}, profit > max_profit: #{profit > max_profit}"
       # puts "==============\n"
+      max_profit = profit if profit > max_profit
     end
   end
 
   max_profit(yesterday_stock_prices[1..-1], max_profit)
 end
 
-puts "result: #{max_profit(blah)}, array: #{blah}"
-puts "result: #{max_profit(poop)}, array: #{poop}"
-puts "result: #{max_profit(p)}, array: #{p}"
+puts "result: #{max_profit(profit_seventy, profit_seventy[1] - profit_seventy[0])}, array: #{profit_seventy}"
+puts "result: #{max_profit(negative_profit, negative_profit[1] - negative_profit[0])}, array: #{negative_profit}"
+puts "result: #{max_profit(profit_six, profit_six[1] - profit_six[0])}, array: #{profit_six}"
