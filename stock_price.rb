4fa6996e9a0b8fe087e5da7ad0564455fdecb4a8
Jacinda Zhong <jacindaz@gmail.com>
2016-12-04 13:29:56 -0800

    Refactor so that do not have to initialize max_profit

--- stock_price.rb
+++ stock_price.rb
@@ -2,8 +2,8 @@ profit_six = [10, 7, 5, 8, 11, 9]
 negative_profit = [100, 50, 0, -50]
 profit_seventy = [99, 119, 50, 120, 20]
 
-
-def max_profit(yesterday_stock_prices, max_profit)
+def max_profit(yesterday_stock_prices, max_profit = nil)
+  max_profit = yesterday_stock_prices[1] - yesterday_stock_prices[0] if max_profit.nil?
   return max_profit if yesterday_stock_prices.length == 1
 
   buy_price = yesterday_stock_prices[0]
@@ -14,15 +14,19 @@ def max_profit(yesterday_stock_prices, max_profit)
       # puts "\n=============="
       # puts "buy_price: #{buy_price}, sell_price: #{sell_price}"
       # puts "yesterday_stock_prices: #{yesterday_stock_prices}"
-      # puts "profit: #{profit}, max_profit: #{max_profit}, profit > max_profit: #{profit > max_profit}"
-      # puts "==============\n"
+      # puts "profit: #{profit}, max_profit: #{max_profit}"
+      # puts "\nprofit > max_profit: #{profit > max_profit}"
+
       max_profit = profit if profit > max_profit
+
+      # puts "new max_profit: #{max_profit}"
+      # puts "==============\n"
     end
   end
 
   max_profit(yesterday_stock_prices[1..-1], max_profit)
 end
 
-puts "result: #{max_profit(profit_seventy, profit_seventy[1] - profit_seventy[0])}, array: #{profit_seventy}"
-puts "result: #{max_profit(negative_profit, negative_profit[1] - negative_profit[0])}, array: #{negative_profit}"
-puts "result: #{max_profit(profit_six, profit_six[1] - profit_six[0])}, array: #{profit_six}"
+puts "result: #{max_profit(profit_seventy)}, array: #{profit_seventy}"
+puts "result: #{max_profit(negative_profit)}, array: #{negative_profit}"
+puts "result: #{max_profit(profit_six)}, array: #{profit_six}"
