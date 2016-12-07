3d34816f73aa0884e2350164a50bb0124b3f1c64
Jacinda Zhong <jacindaz@gmail.com>
2016-12-07 09:20:40 -0800

    Brute force approach

--- new_product_of_other_numbs.rb
+++ new_product_of_other_numbs.rb
@@ -1,3 +1,4 @@
+length_three = [7, 3, 4]
 length_four = [1, 7, 3, 4]
 length_five = [1, 7, 3, 4, 2]
 length_six = [1, 7, 3, 4, 2, 8]
@@ -9,16 +10,33 @@ def product_of_other_numbers(numbers)
   reusable_number_one = numbers[2..(array_length - 1)].reduce(:*)
   reusable_number_two = numbers[0..(array_length - 3)].reduce(:*)
 
-  puts "\n================="
-  puts "reusable_number_one: #{reusable_number_one}"
-  puts "reusable_number_two: #{reusable_number_two}"
-  puts "=================\n"
+  # puts "\n================="
+  # puts "reusable_number_one: #{reusable_number_one}"
+  # puts "reusable_number_two: #{reusable_number_two}"
+  # puts "=================\n"
 
-  numbers.each_with_index do |num, index|
+  products = []
 
+  numbers.each do |current_number|
+    product = 1
+    numbers.each do |multiplying_number|
+      unless multiplying_number == 0 || current_number == multiplying_number
+        product *= multiplying_number
+      end
+    end
+
+    products << product
   end
+
+  puts "\n=============="
+  puts "original array: #{numbers}"
+  puts "products: #{products}"
+  puts "==============\n"
+
+  products
 end
 
+product_of_other_numbers(length_three)
 product_of_other_numbers(length_four)
 product_of_other_numbers(length_five)
 product_of_other_numbers(length_six)
