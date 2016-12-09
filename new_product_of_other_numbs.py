78e12a64e45acbcde17a0a48d04a8a6376785c77
Jacinda Zhong <jacindaz@gmail.com>
2016-12-08 22:25:46 -0800

    Keep track of product before a particular number, to re-use

--- new_product_of_other_numbs.py
+++ new_product_of_other_numbs.py
@@ -1,40 +1,47 @@
-length_three = [7, 3, 4]
+length_three = [7, 3, 4, 0]
 length_four = [1, 7, 3, 4]
 length_five = [1, 7, 3, 4, 2]
 length_six = [1, 7, 3, 4, 2, 8]
 
+test = [1, 2, 6, 5, 9]
+
 # brute force approach
 def product_of_other_numbers(numbers):
-    products = []
-    array_length = len(numbers)
-    # reusable_number_one = numbers[2..(array_length - 1)].reduce(:*)
-    # reusable_number_two = numbers[0..(array_length - 3)].reduce(:*)
-
-    # print("\n=================")
-    # print("reusable_number_one: %s", % reusable_number_one")
-    # print("reusable_number_two: %s", % reusable_number_two")
-    # print("=================\n")
+    reusable_components = { 1: { 'before': 1, 'after': 1 },
+                            2: { 'before': 1, 'after': 1 },
+                            6: { 'before': 1, 'after': 1 },
+                            5: { 'before': 1, 'after': 1 },
+                            9: { 'before': 1, 'after': 1}
+                            }
 
     products = []
-
     for current_number in numbers:
+        # 2*6*5*9
         product = 1
 
-        for multiplying_number in numbers:
-            if multiplying_number != 0 and current_number != multiplying_number:
-                product *= multiplying_number
+        num_is_before = True
+        before_product = 1
+
+        for num in numbers:
+            if current_number == num:
+                num_is_before = False
+                reusable_components[num]['before'] = before_product
+                print("current_number: %d" % current_number)
+                print("num: %d" % num)
+                print("reusable_components: %s" % reusable_components)
+                print("\n")
+            elif num_is_before:
+                before_product *= num
+
+            product *= num
+
+        if current_number == 1:
+            reusable_components[current_number]['after']
 
         products.append(product)
 
-    print("\n==============")
-    print("original array: %s" % numbers)
-    print("products: %s" % products)
-    # print("products: %s, original array: %s" % (products, numbers))
-    print("==============")
-
-    return products
-
-product_of_other_numbers(length_three)
-product_of_other_numbers(length_four)
-product_of_other_numbers(length_five)
-product_of_other_numbers(length_six)
+product_of_other_numbers(test)
+# product_of_other_numbers(length_three)
+# product_of_other_numbers(length_four)
+# product_of_other_numbers(length_five)
+# product_of_other_numbers(length_six)
