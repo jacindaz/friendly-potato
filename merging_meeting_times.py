58c8e89ff3aa88622b0e5b4d0df77a8a0874340a
Jacinda <jacindaz@gmail.com>
2017-01-25 18:17:24 -0800

    Remove extraneous comments and print statements

--- merging_meeting_times.py
+++ merging_meeting_times.py
@@ -2,21 +2,9 @@ import pdb
 
 def merge_ranges(meetings):
     merged_meetings = [meetings[0]]
-    print("\n============")
-    print(merged_meetings)
-    print("============\n")
 
     for index, current_toople in enumerate(meetings):
-
-
         for merged_index, merged_toople in enumerate(merged_meetings):
-            # if current_toople == (2, 3):
-            #     pdb.set_trace()
-            # merged_index == [0]
-            # current_toople (150, 170)
-            # compare current_toople with meetings[merged_toople]
-            #  => (100, 120)
-
             print("\n============\ncurrent toople: ")
             print(current_toople)
             print("merged toople: ")
@@ -28,16 +16,9 @@ def merge_ranges(meetings):
             if index == 0:
                 break
             else:
-                # toople_index_zero = current_toople[0] # 150
-                # toople_index_one = current_toople[1] # 170
-                # merged_toople => (100, 120)
-                # current_toople => (75, 110)
-                # merged_index [0, 1]
-
                 # add toople to the end, once we've checked
                 if current_toople[0] > merged_toople[1] and current_toople[0] > merged_meetings[-1][0] and merged_index == last_index(merged_meetings):
                     merged_meetings.append(current_toople)
-                    # print("\ninside current_toople[0] > merged_toople[1]")
                     break
                 elif current_toople[0] >= merged_toople[0] and current_toople[1] <= merged_toople[1]:
                     break
@@ -53,9 +34,6 @@ def merge_ranges(meetings):
                     merged_meetings[merged_index] = current_toople
                     break
 
-                # shoot, what about when current_toople is in between 2 merged tuples ?
-                # will need to merge all 3 !!!
-                # solve this case later
                 elif merged_index < last_index(merged_meetings) and current_toople[0] >= merged_toople[0] and current_toople[0] < merged_toople[1] and current_toople[1] > merged_toople[1] and current_toople[1] < merged_meetings[merged_index + 1]:
                     merged_meetings[merged_index] = (merged_toople[0], current_toople[1])
                     break
@@ -75,7 +53,6 @@ def merge_ranges(meetings):
                     break
 
                 elif current_toople[0] <= merged_toople[0] and current_toople[1] < merged_toople[1]:
-                    # print("\ninside current_toople[0] <= merged_toople[0] and current_toople[1] <= merged_toople[1]")
 
                     # (100, 120)
                     # (75, 110)
@@ -84,7 +61,6 @@ def merge_ranges(meetings):
                     merged_meetings[merged_index] = newly_merged_toople
                     break
 
-
     print("\n==============")
     print(merged_meetings)
     print("==============\n")
@@ -92,7 +68,6 @@ def merge_ranges(meetings):
 
 def last_index(merged_meetings):
     return (len(merged_meetings) - 1)
-
 
 ranges = [(0, 1), (3, 5), (4, 8), (10, 12), (9, 10)]
 #  => [(0, 1), (3, 8), (9, 12)]
